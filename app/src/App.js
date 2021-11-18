6ee193aacef23bffdb8b9296562b0e7cf9006839
alvaro-78 <alvaro78an@gmail.com>
2021-11-18 13:52:30 +0100

    Add Navbar component

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from '../src/views/Home';
+
 import './App.css';
+// import NavBar from './components/NavBar/NavBar';
 
 function App() {
-    return <div className='App'>Hola</div>;
+    return (
+        <>
+            <div className='App'>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        </>
+    );
 }
 
 export default App;
