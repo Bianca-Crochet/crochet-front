250679de38d556469f8ce561aebf9ea24cbd1723
alvaro-78 <alvaro78an@gmail.com>
2021-11-19 12:50:57 +0100

    Fixed how is the Cards

--- app/src/components/Card/Card.js
+++ app/src/components/Card/Card.js
@@ -4,14 +4,12 @@ const Card = () => {
     return (
         <>
             <div className='col'>
-                <div className='card'>
+                <div className='card h-100'>
                     <img src='...' className='card-img-top' alt='...' />
                     <div className='card-body'>
-                        <h5 className='card-title'>Card title</h5>
-                        <p className='card-text'>
-                            This is a longer card with supporting text below as a natural lead-in to additional content.
-                            This content is a little bit longer.
-                        </p>
+                        <h5 className='card-title'>Nombre del Artículo</h5>
+                        <p className='card-text'>Descripción del Artículo</p>
+                        <p className='card-text'>Precio del Artículo</p>
                     </div>
                 </div>
             </div>
