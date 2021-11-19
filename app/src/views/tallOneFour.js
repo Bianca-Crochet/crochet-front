17efe34e80b42f058377d03a4d1ff73841766283
alvaro-78 <alvaro78an@gmail.com>
2021-11-19 12:21:36 +0100

    Add talls in views

---
+++ app/src/views/tallOneFour.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const tallOneFour = () => {
+    return <div>Tall 1/4</div>;
+};
+
+export default tallOneFour;
---
+++ app/src/views/tallOneSix.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const tallOneSix = () => {
+    return <div>Tall 1/6</div>;
+};
+
+export default tallOneSix;
