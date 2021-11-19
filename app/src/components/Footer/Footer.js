73e8dd49910776f2d5ed17c9257835900fbe1451
alvaro-78 <alvaro78an@gmail.com>
2021-11-19 20:27:15 +0100

    Add Footer Component

--- app/src/components/Footer/Footer.js
+++ app/src/components/Footer/Footer.js
@@ -5,136 +5,44 @@ import classes from './footer.module.css';
 const Footer = () => {
     return (
         <>
-            <footer className={`py-5 ${classes['footer']}`}>
-                <div className='row'>
-                    <div className='col-2'>
-                        <h5>Section</h5>
-                        <ul className='nav flex-column'>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    Home
-                                </a>
-                            </li>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    Features
-                                </a>
-                            </li>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    Pricing
-                                </a>
-                            </li>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    FAQs
-                                </a>
-                            </li>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    About
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className='col-2'>
-                        <h5>Section</h5>
-                        <ul className='nav flex-column'>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    Home
-                                </a>
-                            </li>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    Features
-                                </a>
-                            </li>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    Pricing
-                                </a>
-                            </li>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    FAQs
-                                </a>
-                            </li>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    About
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className='col-2'>
-                        <h5>Section</h5>
-                        <ul className='nav flex-column'>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    Home
-                                </a>
-                            </li>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    Features
-                                </a>
-                            </li>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    Pricing
-                                </a>
-                            </li>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    FAQs
-                                </a>
-                            </li>
-                            <li className='nav-item mb-2'>
-                                <a href='#' className='nav-link p-0 text-muted'>
-                                    About
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className='col-4 offset-1'>
-                        <form>
-                            <h5>Subscribe to our newsletter</h5>
-                            <p>Monthly digest of whats new and exciting from us.</p>
-                            <div className='d-flex w-100 gap-2'>
-                                <label for='newsletter1' className='visually-hidden'>
-                                    Email address
-                                </label>
-                                <input
-                                    id='newsletter1'
-                                    type='text'
-                                    className='form-control'
-                                    placeholder='Email address'
-                                />
-                                <button className='btn btn-primary' type='button'>
-                                    Subscribe
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
-                <div className='d-flex justify-content-center ps-4 py-4 my-4 border-top'>
-                    <p>&copy; 2021 Bianca Crochet Inc. All rights reserved.</p>
-                    <ul className='list-unstyled d-flex'>
-                        <li className='ms-3'>
-                            <a className='link-dark' href='#'>
-                                <svg className='bi' width='24' height='24'>
-                                    <use href='#instagram' />
-                                </svg>
+            <div class='container'>
+                <footer class='py-3 my-4'>
+                    <ul class='nav justify-content-center border-bottom pb-3 mb-3'>
+                        <li class='nav-item'>
+                            <a href='#' class='nav-link px-2 text-muted'>
+                                Home
+                            </a>
+                        </li>
+                        <li class='nav-item'>
+                            <a href='#' class='nav-link px-2 text-muted'>
+                                Features
+                            </a>
+                        </li>
+                        <li class='nav-item'>
+                            <a href='#' class='nav-link px-2 text-muted'>
+                                Pricing
+                            </a>
+                        </li>
+                        <li class='nav-item'>
+                            <a href='#' class='nav-link px-2 text-muted'>
+                                FAQs
+                            </a>
+                        </li>
+                        <li class='nav-item'>
+                            <a href='#' class='nav-link px-2 text-muted'>
+                                About
                             </a>
                         </li>
                     </ul>
-                </div>
-            </footer>
+                    <p class='text-center text-muted'>&copy; 2021 Company, Inc</p>
+                    <div className='d-flex flex-column'>
+                        Síguenos en Redes !!!
+                        <a className={`${classes['icon']}`} href='https://www.instagram.com/bianca_cosaj/'>
+                            <i className='bi bi-instagram'></i>
+                        </a>
+                    </div>
+                </footer>
+            </div>
         </>
     );
 };
