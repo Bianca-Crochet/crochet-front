f078a08b5c5610b493480e6f96db2037edd3206a
alvaro-78 <alvaro78an@gmail.com>
2021-11-22 19:50:38 +0100

    Add props to Card

--- app/src/views/tallOneEight.js
+++ app/src/views/tallOneEight.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Card from '../components/Card/Card';
 
+import Bufanda from '../assets/images/bufanda-crochet.jpeg';
+
+// Está hardcodeado para probarlo como ejemplo, cuando tengamos datos de DB lo renderizaremos con un map
+
 const TallOneEight = () => {
     return (
         <>
             <div className='container pt-4'>
                 <div className='row row-cols-1 row-cols-md-3 g-4'>
-                    <Card />
+                    <Card
+                        source={Bufanda}
+                        name={'Bufanda'}
+                        description={'Preciosa bufanda hecha a mano'}
+                        price={'20€'}
+                        alt={'bufanda'}
+                    />
                     <Card />
                     <Card />
                     <Card />
